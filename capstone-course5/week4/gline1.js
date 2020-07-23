6be75441ef933e4790a2f8a1a596f5d411ec998f
JINMETA <56715909+jinmeta@users.noreply.github.com>
2020-07-23 14:02:23 +0530

    Delete gline1.js

--- capstone-course5/week4/gline1.js
+++
@@ -1,4 +0,0 @@
-gline = [ ['Year','umich.edu','indiana.edu','mac.com','cam.ac.uk','berkeley.edu','uct.ac.za','ucdavis.edu','gmail.com','ufp.pt','yale.edu'],
-['2005',57,12,12,7,12,14,11,10,10,5],
-['2006',455,157,155,152,147,108,109,102,88,91]
-];
