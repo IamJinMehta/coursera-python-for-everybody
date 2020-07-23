4c52eb4f592ea5a6fcff00c6d8abaf84bbbbc7b7
JINMETA <56715909+jinmeta@users.noreply.github.com>
2020-07-23 12:47:52 +0530

    Create Counting Email in a Database.py

---
+++ course4/week2/Counting Email in a Database.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+conn=sqlite3.connect('emaildb.sqlite')
+cur=conn.cursor()
+
+cur.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='Counts' ''')
+if cur.fetchone()[0]==1 : 
+	print("Table FOUND")
+cur.execute('DROP TABLE IF EXISTS Counts')
+print("Table droped........")
+cur.execute('''CREATE TABLE Counts(email TEXT,count INTEGER)''')
+print("Creating Table->Counts\nwait.......")
+print("======================Table Created======================")
+
+fname=input('Enter file name: ')
+if(len(fname)<1): fname ='mbox-short.txt'
+fh=open(fname)
+print("Reading:",fname,"\nplease wait.....")
+for line in fh:
+	if not line.startswith('From: '): continue
+	pieces=line.split()
+	email=pieces[1]
+	cur.execute('SELECT count FROM Counts WHERE email = ?',(email,))
+	row=cur.fetchone()
+	if row is None:
+		cur.execute('''INSERT INTO Counts (email, count) VALUES (?,1)''',(email,))
+	else:
+		cur.execute('UPDATE Counts SET count=count+1 WHERE email = ?',(email,))
+
+conn.commit()
+print("++++++++++++++++++++++=commit done=+++++++++++++++++++++++")
+
+sqlstr='SELECT email,count FROM Counts ORDER BY count DESC'
+
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
+
+cur.close()
